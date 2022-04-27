3fce72154179ad29c0ce7375f2bd20a40434285a
woodi97 <woodi.daily@gmail.com>
2022-04-27 16:05:22 +0900

    Init: Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@styles/pages/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.cnt}>
+      <div>Init</div>
+    </div>
+  )
+}
+
+export default Home
