4bf19a970aa7584336bc69ad824f078b2b292757
haryung-lee <lhr4884@naver.com>
2022-04-28 04:02:47 +0900

    Feat: publishing profile page

--- instagram/src/components/common/ProfileIcon.tsx
+++ instagram/src/components/common/ProfileIcon.tsx
@@ -2,62 +2,66 @@ import { getRandomInt } from "@src/utils/number";
 import { FC, memo } from "react";
 import Image from "./ImageWrapper";
 
-type iconSize = "small" | "medium" | "large";
+type iconSize = "small" | "medium" | "large" | "xlarge";
 
 type iconShape = {
-  iconSize?: iconSize;
-  storyBorder?: boolean;
-  image?: string;
+	iconSize?: iconSize;
+	storyBorder?: boolean;
+	image?: string;
 };
 
 const iconSizeSelector: {
-  [keys in iconSize]: { width: number; height: number };
+	[keys in iconSize]: { width: number; height: number };
 } = {
-  small: {
-    width: 25,
-    height: 25,
-  },
-  medium: {
-    width: 35,
-    height: 35,
-  },
-  large: {
-    width: 60,
-    height: 60,
-  },
+	small: {
+		width: 25,
+		height: 25,
+	},
+	medium: {
+		width: 35,
+		height: 35,
+	},
+	large: {
+		width: 60,
+		height: 60,
+	},
+	xlarge: {
+		width: 145,
+		height: 145,
+	},
 };
 
 const ProfileIcon: FC<iconShape> = ({
-  iconSize = "medium",
-  storyBorder,
-  image,
+	iconSize = "medium",
+	storyBorder,
+	image,
 }) => {
-  const randomId = getRandomInt(1, 70);
-  const size = iconSizeSelector[iconSize];
-  const profileImage = image
-    ? image
-    : `https://i.pravatar.cc/150?img=${randomId}`;
+	const randomId = getRandomInt(1, 70);
+	const size = iconSizeSelector[iconSize];
+	const profileImage = image
+		? image
+		: `https://i.pravatar.cc/150?img=${randomId}`;
 
-  return (
-    <span
-      className={
-        storyBorder
-          ? "rounded-full p-1 m-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-          : ""
-      }
-    >
-      <div className="flex items-center border-solid border-white border-[1px] rounded-full hover:cursor-pointer">
-        <Image
-          className="rounded-full border-sm"
-          width={size.width}
-          height={size.height}
-          layout="fixed"
-          src={profileImage}
-          alt="profile"
-        />
-      </div>
-    </span>
-  );
+	return (
+		<span
+			className={
+				storyBorder
+					? "rounded-full p-1 m-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+					: ""
+			}
+		>
+			<div className="flex items-center border-solid border-white border-[1px] rounded-full hover:cursor-pointer">
+				<Image
+					className="rounded-full border-sm"
+					width={size.width}
+					height={size.height}
+					layout="fixed"
+					src={profileImage}
+					alt="profile"
+				/>
+			</div>
+		</span>
+	);
 };
 
 export default memo(ProfileIcon);
--- instagram/src/pages/profile.tsx
+++ instagram/src/pages/profile.tsx
@@ -1,20 +1,58 @@
 import { HorizontalLine } from "@src/components/common";
 import Image from "@src/components/common/ImageWrapper";
 import React from "react";
+import ProfileIcon from "@src/components/common/ProfileIcon";
 
 type Props = {};
 
 const profile = (props: Props) => {
-  return (
-    <div className="flex w-full">
-      <div className="relative w-1/2 h-[400px]">
-        <Image src="/vercel.svg" layout="fill" alt="" />
-      </div>
-      <div className="relative w-1/2 h-[400px]">
-        <Image src="/vercel.svg" layout="fill" alt="" />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex py-10 w-full">
+				<div className="w-1/3 mr-3">
+					<ProfileIcon iconSize="xlarge" />
+				</div>
+				<section className="w-2/3">
+					<div className="flex w-full gap-x-3 mb-4">
+						<h3 className="text-3xl">nickname</h3>
+						<button className="bg-white text-gray-800 font-semibold py-0.5 px-1 border border-gray-300 rounded">
+							프로필편집
+						</button>
+					</div>
+					<ul className="flex w-full gap-x-7 mb-4">
+						<li>
+							게시물 <span className="font-semibold">1</span>
+						</li>
+						<li>
+							팔로워 <span className="font-semibold">100</span>
+						</li>
+						<li>
+							팔로우 <span className="font-semibold">150</span>
+						</li>
+					</ul>
+					<div>
+						<span className="font-semibold">이름</span>
+						<div>자기소개</div>
+					</div>
+				</section>
+			</div>
+			<HorizontalLine />
+			<div className="grid grid-cols-3 gap-6 py-10 w-full">
+				<div className="relative h-[256px] hover:bg-gray-200 cursor-pointer">
+					<Image src="/vercel.svg" layout="fill" alt="" />
+				</div>
+				<div className="relative h-[256px] hover:bg-gray-200 cursor-pointer">
+					<Image src="/vercel.svg" layout="fill" alt="" />
+				</div>
+				<div className="relative h-[256px] hover:bg-gray-200 cursor-pointer">
+					<Image src="/vercel.svg" layout="fill" alt="" />
+				</div>
+				<div className="relative h-[256px] hover:bg-gray-200 cursor-pointer">
+					<Image src="/vercel.svg" layout="fill" alt="" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default profile;
